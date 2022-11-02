966812772cfef971197690ff95294315dfffbb1b
Andreas Eidmann <eidmann@obm.at>
2022-11-02 01:07:14 +0100

    svelte

--- esplog/json.ino
+++ esplog/json.ino
@@ -7,18 +7,28 @@ void handleJsonData()
     {
         for( int j = 0; j < (sizeof(queryRealName)/sizeof(queryRealName[0])); j++ )
         {
-        if( strncmp(ausgabe[i].realName,queryRealName[j],20) == 0 )
-        {
-            doc[ausgabe[i].realName] = ausgabe[i].printValue;
+        //if( strncmp(ausgabe[i].realName,queryRealName[j],20) == 0 )
+        //{   
+            JsonObject obj1 = doc.createNestedObject();
+            obj1["title"] = ausgabe[i].realName;
+            obj1["value"] = ausgabe[i].printValue;
+            obj1["unit"] = ausgabe[i].unit;
+            // doc.add(obj1);
             break;
-        }
+        //}
         }
     }
+
+    JsonObject obj1 = doc.createNestedObject();
+    obj1["title"] = "Free RAM";
+    obj1["value"] = String(ESP.getFreeHeap());
+    obj1["unit"] = "Byte";
 
     String output = "";
     serializeJson(doc, output);
     server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     server.sendHeader("Pragma", "no-cache");
+    server.sendHeader("Access-Control-Allow-Origin", "*");
     server.setContentLength(CONTENT_LENGTH_UNKNOWN);
     server.send(200, "text/json", output);
   
--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,48 @@
 <script>
-   import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
-  import IconButton from '@smui/icon-button';
-  
-  import Button, { Label } from '@smui/button';
+  import { onMount } from "svelte";
+  import axios from "axios";
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+  import List, { Item, Text, PrimaryText, SecondaryText } from "@smui/list";
+  import IconButton from "@smui/icon-button";
+  import CircularProgress from "@smui/circular-progress";
+
+  import Button, { Label } from "@smui/button";
 
   let clicked = 0;
+
+  let dataList;
+
+  // let server = "http://192.168.0.89/data";
+
+  let server = "/data";
+
+  onMount(() => {
+    loadData();
+  });
+
+  const loadData = () => {
+    axios.get(server).then((response) => {
+      console.log(response.data);
+      dataList = response.data;
+    });
+  };
 </script>
 
 <main>
-
   <div class="top-app-bar-container">
-    <TopAppBar
-      variant="static"      
-      color='primary'
-    >
+    <TopAppBar variant="static" color="primary">
       <Row>
         <Section>
           <IconButton class="material-icons">menu</IconButton>
-          <Title>Static</Title>
+          <Title>Wärmepumpe</Title>
         </Section>
         <Section align="end" toolbar>
-          <IconButton class="material-icons" aria-label="Download"
+          <!-- <IconButton class="material-icons" aria-label="Download"
             >file_download</IconButton
           >
           <IconButton class="material-icons" aria-label="Print this page"
             >print</IconButton
-          >
+          > -->
           <IconButton class="material-icons" aria-label="Bookmark this page"
             >bookmark</IconButton
           >
@@ -34,19 +51,29 @@
     </TopAppBar>
     <div>
       <div>
-        <Button on:click={() => clicked++}>
-          <Label>Default</Label>
-        </Button>
-      </div>     
+        {#if dataList}
+          <List twoLine nonInteractive>
+            {#each dataList as data}
+              <Item>
+                <Text>
+                  <PrimaryText>{data.title}</PrimaryText>
+                  <SecondaryText>{data.value + "[" + data.unit + "]"}</SecondaryText>                  
+                </Text>
+              </Item>
+            {/each}
+          </List>
+        {:else}
+          <div style="display: flex; justify-content: center">
+            <CircularProgress
+              style="height: 32px; width: 32px;"
+              indeterminate
+            />
+          </div>
+        {/if}
+      </div>
     </div>
   </div>
-  
-
- 
-
-  
 </main>
 
 <style>
-  
 </style>
